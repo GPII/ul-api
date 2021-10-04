d68a60f94232675c963f6b3a4ee3b92122b9f304
Tony Atkins <tony@raisingthefloor.org>
2021-10-04 11:57:25 +0200

    Filtered iso code view to only work with published records.

--- src/couchapp/eastin/views/byisocode/map.js
+++ src/couchapp/eastin/views/byisocode/map.js
@@ -1,6 +1,6 @@
 function (doc) {
     "use strict";
-    if (doc.source === "unified" && doc.isoCodes && doc.isoCodes.length > 0)  {
+    if (doc.status !== "deleted" && doc.status !== "new" && doc.source === "unified" && doc.isoCodes && doc.isoCodes.length > 0)  {
         for (var index = 0; index < doc.isoCodes.length; index ++) {
             var isoCodeDef = doc.isoCodes[index];
             if (isoCodeDef.Code) {
